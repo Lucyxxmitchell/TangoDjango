0c8dacbd0c9d54f3b4213ce4a9022eb26a8615e5
Lucyxxmitchell <2646175m@student.gla.ac.uk>
2022-03-23 15:33:34 +0000

    Completed chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category #imports the category model
+from rango.models import Page
 
 def index(request):
-    #construct a dictionary to pass the template engine as its context.
-    #note the key boldmessage matches to {{  boldmessage  }} in the template!
-    context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
-    #return a rendered response to the client
-    #we make use of the shortcuut function t make our lives easier.
-    #note that the first parameter is the template we wish to use.
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
     return render(request, 'rango/index.html', context=context_dict)
 
 
@@ -18,3 +21,20 @@ def about(request):
     # note that the first parameter is the template we wish to use.
     return render(request, 'rango/about.html')
 
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
